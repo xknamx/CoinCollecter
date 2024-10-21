3942186567143d72ea6c00948458b43a7422fe56
ChiyunaZen <uni202406_11@telework.to>
2024-10-21 13:11:34 +0900

    Walk

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,39 +4,96 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 0.01f; //floatは小数点
-    private Animator animator;
+    [SerializeField] private float speed = 1.0f;    //移動速度
+    [SerializeField] private Animator animator;     //Animatorのセット
+    Vector2 PlayerPos; //プレイヤーの位置
+    float horizontal;
+    SpriteRenderer spriteRenderer;
+   //  bool isWalking = false;
+
+
 
     private void Start()
     {
         animator = GetComponent<Animator>();
+        PlayerPos = transform.position;
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        Vector2 pos = transform.position;
-        if (Input.GetKey(KeyCode.DownArrow))//↓キーを押したら
+
+        PlayerMoving();
+
+    }
+
+    //プレイヤーの移動処理
+    void PlayerMoving()
+    {
+        float horizontal = Input.GetAxis("Horizontal"); // ←→またはADキー入力を検出
+        float vertical = Input.GetAxis("Vertical");     // ↑↓またはWSキー入力を検出
+
+        if (horizontal != 0)
         {
-            pos.y -= speed;
-            animator.SetInteger("direction", 0);//正面を向く
+            if (horizontal > 0) //左入力されたとき
+            {
+                spriteRenderer.flipX = false; //左を向く
+            }
+            else if (horizontal < 0) //右入力されたとき
+            {
+                spriteRenderer.flipX = true; //右を向く
+            }
+            PlayerPos.x += horizontal * speed ;
+            animator.SetFloat("speed", 1.0f);
+            
         }
-        else if (Input.GetKey(KeyCode.LeftArrow))//←キーを押したら
+        else
+        {
+            animator.SetFloat("speed", 0.0f);
+        }
+
+      
+        transform.position = PlayerPos; //プレイヤーの位置を更新
+
+        ////前後方向
+        //if (vertical > 0)
+        //{
+        //    animator.SetInteger("direction", 3);//後ろを向く
+        //}
+
+        //if (vertical < 0) //前を向く
+        //{
+        //    animator.SetInteger("direction", 0);//正面を向く
+        //}
+
+
+        // 入力がある場合はアニメーションを再生
+        //if (horizontal != 0 || vertical != 0)
+        //{
+        //    animator.SetFloat("MoveSpeed", 1f); // アニメーション再生
+        //}
+        //else
+        //{
+        //    animator.SetFloat("MoveSpeed", 0f); // アニメーション停止
+        //}
+        //PlayerPos.x += horizontal * speed * Time.deltaTime;
+        //transform.position = PlayerPos; //プレイヤーの位置を更新
+    }
+
+
+
+    //自販機を探す際の処理
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("VendingMachine"))
         {
 
-            pos.x -= speed;
-            animator.SetInteger("direction", 1);//左を向く
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                animator.SetInteger("direction", 3); //自販機のほうを向く
+                Debug.Log("自販機をクリックしました");
+                //自販機を探す処理を追加
+            }
         }
-        else if (Input.GetKey(KeyCode.RightArrow))//→キーを押したら
-        {
-            pos.x += speed;
-            animator.SetInteger("direction", 2);//右を向く
-
-        }
-        else if (Input.GetKey(KeyCode.UpArrow))//↑キーを押したら
-        {
-            pos.y += speed;
-            animator.SetInteger("direction", 3);//後ろを向く
-        }
-        transform.position = pos;
     }
 }
