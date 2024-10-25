35a7b7770fdc8cfd69090c91ea08464e5d4fdc13
KaedeOshima <uni202406_03@telework.to>
2024-10-25 09:43:59 +0900

    タイトル画面実装

---
+++ Assets/Script/TitleAnimator.cs
@@ -0,0 +1,28 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleAnimator : MonoBehaviour
+{
+
+    void Start()
+    {
+
+            // 回転と揺れのアニメーションをシーケンスで作成
+            Sequence mySequence = DOTween.Sequence();
+
+            // 「びよん」と回転させるアニメーション (FastBeyond360 で一回転以上)
+            mySequence.Append(transform.DORotate(new Vector3(0, 0, -90), 1f, RotateMode.FastBeyond360));
+
+            // 「ゆらゆら」と揺らすアニメーション（Punch）
+            mySequence.Append(transform.DOPunchRotation(new Vector3(50f, 0f, 50f), 5f, 2, 30f));
+
+            // シーケンスを再生
+            mySequence.Play();
+        }
+    }
+
+
+
